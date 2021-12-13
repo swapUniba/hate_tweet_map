f32b23259edc16e2b5ccabdf7734e9bb80c13909
Alessia Laforgia <84467216+AlessiaLa@users.noreply.github.com>
2021-12-13 18:06:24 +0100

    Updated rules

--- script/inclusivity_management/Rules.py
+++ script/inclusivity_management/Rules.py
@@ -14,12 +14,16 @@ def check_female_name(token):
         return False
 
 
-def articolo_nome(tweet, inclusive):
+def articolo_nome(tweet):
     for idx,(token,tag,det, morph) in enumerate(tweet):
         if tag == 'PROPN' and idx != 0:
             if tweet[idx-1][1] == 'DET':
-                if morph[idx-1]['Gender'] == 'Fem':
-                    inclusive = inclusive - 0.25
+                if morph !={}:
+                    if 'Gender' in morph:
+                        if morph[idx-1]['Gender'] == 'Fem':
+                            inclusive = - 0.25
+        else:
+            inclusive = 0
     return inclusive
 
 
@@ -28,37 +32,48 @@ def articolo_nome(tweet, inclusive):
 ## det == word[2]
 ## new_morph[pos]['gender']== word[3]['Gender']
 
-def femaleName_maleAppos(tweet, inclusive):
+def femaleName_maleAppos(tweet):
     for idx,(token, tag, det, morph) in enumerate(tweet):
         if tag == 'PROPN' and check_female_name(token):
             continue
         if tag == 'NOUN' and det == 'appos':
-            if morph['Gender'] == 'Masc':
-                inclusive = inclusive - 0.25
+            if 'Gender' in morph:
+                if morph['Gender'] == 'Masc':
+                    inclusive = - 0.25
+
+        else:
+            inclusive = 0
     return inclusive
 
 
-def art_donna_noun(tweet, inclusive):
+def art_donna_noun(tweet):
     for idx,(token,tag,det, morph) in enumerate(tweet):
         if token == 'donna' and idx != 0:
             if det == 'DET':
                 if morph['Gender'] == 'Fem':
                     if tweet[idx+1][1] == 'NOUN' and tweet[idx+1][3]['Gender'] == 'Masc':
-                        inclusive = inclusive - 0.25
+                        inclusive = - 0.25
+        else:
+            inclusive=0
+
     return inclusive
 
-def femaleSub_malePart(tweet, inclusive):
+def femaleSub_malePart(tweet):
     for idx,(token,tag,det, morph) in enumerate(tweet):
         if tag == 'PROPN' or tag == 'NOUN' and det == 'nsubj':
             continue
         if tag == 'AUX' and det == 'aux':
-            if morph['Person'] == '3' and morph['Number'] == 'Sing':
-                if tweet[idx+1][1] == 'VERB' and tweet[idx+1][2] == 'ROOT':
-                    if tweet[idx+1][3]['Gender'] == 'Masc' and tweet[idx+1][3]['VerbForm'] == 'Part' and morph['Number'] == 'Sing':
-                        inclusive = inclusive - 0.25
+            if 'Person' in morph:
+                if morph['Person'] == '3' and morph['Number'] == 'Sing':
+                    if tweet[idx+1][1] == 'VERB' and tweet[idx+1][2] == 'ROOT':
+                        if 'Gender' in morph and 'VerbForm'in morph and 'Number' in morph:
+                            if tweet[idx+1][3]['Gender'] == 'Masc' and tweet[idx+1][3]['VerbForm'] == 'Part' and morph['Number'] == 'Sing':
+                                inclusive = - 0.25
+        else:
+            inclusive=0
     return inclusive
 
-def article_inclusive(tweet, inclusive):
+def article_inclusive(tweet):
     for idx, (token, tag, det, morph) in enumerate(tweet):
         if tag == 'PRON' or tag == 'NOUN':
             if 'Gender' in morph:
@@ -86,15 +101,12 @@ if __name__ == "__main__":
     # Estrazione del Morph
     #print(rp.runtimePos(path)[0][0][3])
 
-    inclusive = 0.0
-    print(rp.runtimePos(path)[0])
-    inclusive += articolo_nome(rp.runtimePos(path)[0], inclusive)
-    inclusive += femaleName_maleAppos(rp.runtimePos(path)[0], inclusive)
-    inclusive += art_donna_noun(rp.runtimePos(path)[0], inclusive)
-    inclusive += femaleSub_malePart(rp.runtimePos(path)[0], inclusive)
-    inclusive += article_inclusive(rp.runtimePos(path)[0], inclusive)
-    print(inclusive)
-
     ## inclusivity for the whole dataset
-    # for phrase in rp.runtimePos(path):
-    #     inclusive = articolo_nome(phrase, inclusive)
+    inclusive = 0.0
+    for phrase in rp.runtimePos(path):
+        inclusive += articolo_nome(phrase)
+        inclusive += femaleName_maleAppos(phrase)
+        inclusive += art_donna_noun(phrase)
+        inclusive += femaleSub_malePart(phrase)
+        inclusive += article_inclusive(phrase)
+    print(inclusive)
